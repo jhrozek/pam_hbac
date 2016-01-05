fa4602e3fc186482e39d56f0c1724b1a198c4516
Jakub Hrozek <jakub.hrozek@gmail.com>
2016-01-05 11:54:08 +0100

    No more gmail

--- src/pam_hbac_compat.h
+++ src/pam_hbac_compat.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014 Jakub Hrozek <jakub.hrozek@gmail.com>
+    Copyright (C) 2014 Jakub Hrozek <jakub.hrozek@posteo.se>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
