87b0e88dfeaada94d2e70374da99f2b3e930e5dc
Jakub Hrozek <jakub.hrozek@posteo.se>
2016-02-24 18:11:02 +0100

    Some platforms require GNU_SOURCE for strnup()

--- src/pam_hbac_dnparse.c
+++ src/pam_hbac_dnparse.c
@@ -15,6 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define _GNU_SOURCE     /* strndup() */
+
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
